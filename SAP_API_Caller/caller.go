9f5783b6e6dd1ac5eec7d7b01e7de7482b3f9fc1
Yurino Oga <yurino.o@latona.io>
2022-09-09 16:24:14 +0900

    release

---
+++ SAP_API_Caller/caller.go
@@ -0,0 +1,211 @@
+package sap_api_caller
+
+import (
+	"fmt"
+	"io/ioutil"
+	sap_api_output_formatter "sap-api-integrations-sales-order-reads/SAP_API_Output_Formatter"
+	"strings"
+	"sync"
+
+	sap_api_request_client_header_setup "github.com/latonaio/sap-api-request-client-header-setup"
+
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
+)
+
+type SAPAPICaller struct {
+	baseURL         string
+	sapClientNumber string
+	requestClient   *sap_api_request_client_header_setup.SAPRequestClient
+	log             *logger.Logger
+}
+
+func NewSAPAPICaller(baseUrl, sapClientNumber string, requestClient *sap_api_request_client_header_setup.SAPRequestClient, l *logger.Logger) *SAPAPICaller {
+	return &SAPAPICaller{
+		baseURL:         baseUrl,
+		requestClient:   requestClient,
+		sapClientNumber: sapClientNumber,
+		log:             l,
+	}
+}
+
+func (c *SAPAPICaller) AsyncGetSalesOrder(salesOrder, salesOrderItem string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "Header":
+			func() {
+				c.Header(salesOrder)
+				wg.Done()
+			}()
+		case "Item":
+			func() {
+				c.Item(salesOrder, salesOrderItem)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *SAPAPICaller) Header(salesOrder string) {
+	headerData, err := c.callSalesOrderSrvAPIRequirementHeader("A_SalesOrder", salesOrder)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(headerData)
+
+	headerPartnerData, err := c.callToHeaderPartner(headerData[0].ToHeaderPartner)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(headerPartnerData)
+
+	itemData, err := c.callToItem(headerData[0].ToHeaderPartner)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemData)
+}
+
+func (c *SAPAPICaller) callSalesOrderSrvAPIRequirementHeader(api, salesOrder string) ([]sap_api_output_formatter.Header, error) {
+	url := strings.Join([]string{c.baseURL, "API_SALES_ORDER_SRV", api}, "/")
+	param := c.getQueryWithHeader(map[string]string{}, salesOrder)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToHeader(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToHeaderPartner(url string) ([]sap_api_output_formatter.HeaderPartner, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToHeaderPartner(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItem(url string) ([]sap_api_output_formatter.Item, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItem(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) Item(salesOrder, salesOrderItem string) {
+	itemData, err := c.callSalesOrderSrvAPIRequirementItem("A_SalesOrderItem", salesOrder, salesOrderItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemData)
+
+	itemPricingElementData, err := c.callToItemPricingElement(itemData[0].ItemPricingElement)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemPricingElementData)
+
+	itemScheduleLineData, err := c.callToItemScheduleLine(itemData[0].ItemScheduleLine)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemScheduleLineData)
+}
+
+func (c *SAPAPICaller) callSalesOrderSrvAPIRequirementItem(api, salesOrder, salesOrderItem string) ([]sap_api_output_formatter.Item, error) {
+	url := strings.Join([]string{c.baseURL, "API_SALES_ORDER_SRV", api}, "/")
+
+	param := c.getQueryWithItem(map[string]string{}, salesOrder, salesOrderItem)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItem(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItemPricingElement(url string) ([]sap_api_output_formatter.ItemPricingElement, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItemPricingElement(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItemScheduleLine(url string) ([]sap_api_output_formatter.ItemScheduleLine, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItemScheduleLine(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) getQueryWithHeader(params map[string]string, salesOrder string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("SalesOrder eq '%s'", salesOrder)
+	return params
+}
+
+func (c *SAPAPICaller) getQueryWithItem(params map[string]string, salesOrder, salesOrderItem string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("SalesOrder eq '%s' and SalesOrderItem eq '%s'", salesOrder, salesOrderItem)
+	return params
+}
